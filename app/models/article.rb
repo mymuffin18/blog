49441eae6c1c1e177b115357a98612d8887b981d
Josh Uy <angatngheaven@gmail.com>
2022-01-28 22:32:15 +0800

    added article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+
+    validates :title, presence: true, uniqueness: { case_sensitive: true }, length: {maximum: 50}
+    validates :body, presence: true, length: {minimum: 5} 
 end
--- db/migrate/20220128141723_create_articles.rb
+++ db/migrate/20220128141723_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[7.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :body
+      t.text :body
 
       t.timestamps
     end
